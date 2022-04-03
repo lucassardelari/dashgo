adcd7e4b7f694ea82826549f796a59441b6a14b8
Lucas Sardelari <lucas_sardelari@hotmail.com>
2022-04-03 19:12:09 -0300

    feat: Apply Header responsive

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,16 +1,23 @@
 import { Flex, Text, Box, Avatar } from '@chakra-ui/react'
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
     return (
         <Flex
             align="center"
         >
-            <Box mr="4" textAlign="right">
-                <Text>Lucas Sardelari</Text>
-                <Text color="gray.300" fontSize="small">
-                    lucas@sardelari.com
-                </Text>
-            </Box>
+            {showProfileData && (
+                <Box mr="4" textAlign="right">
+                    <Text>Lucas Sardelari</Text>
+                    <Text color="gray.300" fontSize="small">
+                        lucas@sardelari.com
+                    </Text>
+                </Box>
+            )}
+
             <Avatar size="md" name="Lucas Sardelari" src="https://github.com/lucassardelari.png" />
         </Flex>
     )
