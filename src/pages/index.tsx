50da6a7be8617b064ab30cfc6c0d7d66ce226aea
Lucas Sardelari <lucas_sardelari@hotmail.com>
2022-04-03 20:55:34 -0300

    feat: install react hook form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,19 @@
 import { Flex, Button, Stack } from "@chakra-ui/react"
+import { SubmitHandler, useForm } from 'react-hook-form'
 import { Input } from "../components/Form/Input"
 
-export default function Home() {
+type SignInFormData = {
+  email: string;
+  password: string;
+}
+
+export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm()
+
+  const handleSignIn:SubmitHandler<SignInFormData> = (values) => {
+    console.log(values)
+  }
+
   return (
     <Flex
       w="100vw"
@@ -17,10 +29,11 @@ export default function Home() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing={4}>
-          <Input name="email" type="email" label="E-mail" />
-          <Input name="password" type="password" label="Senha" />
+          <Input name="email" type="email" label="E-mail" {...register('email')}/>
+          <Input name="password" type="password" label="Senha" {...register('senha')}/>
         </Stack>
 
         <Button
@@ -28,6 +41,7 @@ export default function Home() {
           mt="6"
           colorScheme="pink"
           size="lg"
+          isLoading={formState.isSubmitting}
         >
           Entrar
         </Button>
